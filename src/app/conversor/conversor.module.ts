c2901c4a599e07500ca26e1013dda5b6a09b1c8c
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2022-08-30 12:06:25 -0300

    bootstrap,jquery, modulo, submodulos, models, components,service

---
+++ src/app/conversor/conversor.module.ts
@@ -0,0 +1,21 @@
+import { MoedaService } from './services';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ConversorComponent } from './components';
+
+@NgModule({
+  declarations: [
+    ConversorComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    ConversorComponent
+  ],
+  providers:[
+    MoedaService
+  ]
+})
+export class ConversorModule { }
